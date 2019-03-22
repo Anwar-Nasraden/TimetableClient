69c70e145bb35a27ccbb9872cc13520b6c3a59cc
Anwar Nasraden <anesredin638@gmail.com>
2019-03-22 16:02:08 +0000

    first commit

---
+++ TimetableClient/Client.cs
@@ -0,0 +1,64 @@
+﻿// Console client for Forum service
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTableClientSide
+{
+    class Client
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static readonly String baseURI = "https://localhost:44340/api/Timetables/";
+
+
+        // add a stock 
+      
+
+        static async Task RunGets()
+        {
+            try
+            {
+               
+
+                // GET /api/forum/all
+                var streamTask = client.GetStreamAsync(baseURI);
+                //var serializer = new DataContractJsonSerializer(typeof(List<Timetable>));
+                //var posts = serializer.ReadObject(await streamTask) as List<Timetable>;
+                var serializer1 = new DataContractJsonSerializer(typeof(List<Room>));
+                var rooms = serializer1.ReadObject(await streamTask) as List<Room>;
+                Console.WriteLine("all timetables");
+                //foreach (var p in posts)
+                //{
+                //    Console.WriteLine(p.Room.Room_no);
+                //}
+                Console.WriteLine("List of room numbers");
+                foreach (var r in rooms)
+                {
+                    Console.WriteLine(r);
+                }
+
+
+                // GET /api/forum/last/3
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // kick off
+        static void Main()
+        {
+            
+            RunGets().Wait();
+            client.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TimetableClient/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace TimeTableClientSide
+{
+    [DataContract]
+    public class Timetable
+    {
+        [Key]
+        [DataMember(Name = "Timetable_Id")]
+        public int Timetable_Id { get; set; }
+        [DataMember(Name = "StartTime")]
+        public DateTime StartTime { get; set; }
+        [DataMember(Name = "EndTime")]
+        public DateTime EndTime { get; set; }
+        [DataMember(Name = "Room")]
+        public Room Room { get; set; }
+        [DataMember(Name = "Calendar")]
+        public Calendar Calendar { get; set; }
+
+        [ForeignKey("Calendar_Id")]
+        public int Calendar_Id { get; set; }
+
+        [ForeignKey("Room_Id")]
+        public int Room_Id { get; set; }
+
+    }
+
+
+    [DataContract]
+    public class Room
+    {
+        [Key]
+        [DataMember(Name = "Room_Id")]
+        public int Room_Id { get; set; }
+
+        [DataMember(Name = "Room_no")]
+        public int Room_no { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Description { get; set; }
+    }
+
+    [DataContract]
+    public class Calendar
+    {
+        [Key]
+        [DataMember(Name = "Calendar_Id")]
+        public int Calendar_Id { get; set; }
+        [DataMember(Name = "CalendarDate")]
+        public DateTime CalendarDate { get; set; }
+        [DataMember(Name = "DayofWeek")]
+        public string DayofWeek { get; set; }
+        [DataMember(Name = "WeekNumber")]
+        public int WeekNumber { get; set; }
+
+    }
+}
